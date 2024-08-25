397858fb8a37c3c69875cf0b34ae642599c80321
realquicker <agilestarskim@gmail.com>
2024-08-25 13:02:19 +0900

    products: graphQL과 gatsby Img 적용

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,44 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
+  siteMetadata: {
+    products: [
+      {
+          image: "products-workstation.png",
+          title: "Workstation",
+          description: "최고의 사무용 워크스테이션을 만나보세요."
+      },
+      {
+          image: "products-executive.png",
+          title: "Executive Desk",
+          description: "고급스러운 임원용 책상으로 품격을 더하세요."
+      },
+      {
+          image: "products-partition.png",
+          title: "Partition",
+          description: "프라이버시를 지켜주는 견고한 파티션."
+      },
+      {
+          image: "products-storage.png",
+          title: "Storage",
+          description: "넉넉한 수납 공간으로 사무실을 깔끔하게 정리하세요."
+      },
+      {
+          image: "products-table.png",
+          title: "Table",
+          description: "다양한 용도로 활용 가능한 다목적 테이블."
+      }
+    ]
+  },
+  plugins: [
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    }
+  ],
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,20 +1,44 @@
 import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
+import Img from "gatsby-image";
 import "./Products.css";
 import { Card, Row, Col, Container } from "react-bootstrap";
-import workstationImg from "../images/products-workstation.png";
-import executiveImg from "../images/products-executive.png";
-import partitionImg from "../images/products-partition.png";
-import storageImg from "../images/products-storage.png";
-import tableImg from "../images/products-table.png";
 
-export default function Products() {
-    const images = [
-        workstationImg,
-        executiveImg,
-        partitionImg,
-        storageImg,
-        tableImg
-    ];
+
+const Products = () => {
+    const data = useStaticQuery(graphql`
+        query {
+          site {
+            siteMetadata {
+              products {
+                image
+                title
+                description
+              }
+            }
+          }
+          allFile(filter: { sourceInstanceName: { eq: "images" } }) {
+            edges {
+              node {
+                relativePath
+                childImageSharp {
+                  fluid(maxWidth: 300) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+          }
+        }
+      `);
+
+    const products = data.site.siteMetadata.products;
+    const images = data.allFile.edges;
+
+    const getImage = (imageName) => {
+        const image = images.find(({ node }) => node.relativePath === imageName);
+        return image ? image.node.childImageSharp.fluid : null;
+    };
 
     return (
         <Container fluid>
@@ -25,24 +49,21 @@ export default function Products() {
                 </Col>
             </Row>
             <Row xs={1} md={2} lg={3} className="g-5 justify-content-center">
-                {images.map((image, idx) => (
-                    <Col key={idx}>                    
-                        <Card className="custom-card hover-zoom">
-                            <Card.Img 
-                                variant="top" 
-                                src={image} 
+                {products.map((product, idx) => (
+                    <Col key={idx} className="d-flex">                    
+                        <Card className="custom-card hover-zoom flex-grow-1">
+                            <Img 
+                                fluid={getImage(product.image)} 
+                                alt={product.title}
                                 style={{ 
-                                    height: "300px", 
-                                    width: "100%", 
-                                    objectFit: "cover" 
+                                height: "300px", 
+                                width: "100%", 
+                                objectFit: "cover" 
                                 }}
                             />
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
-                                </Card.Text>
+                            <Card.Body className="d-flex flex-column">
+                                <Card.Title>{product.title}</Card.Title>
+                                <Card.Text>{product.description}</Card.Text>
                             </Card.Body>
                         </Card>
                     </Col>
@@ -51,3 +72,5 @@ export default function Products() {
         </Container>
     );
 }
+
+export default Products;
